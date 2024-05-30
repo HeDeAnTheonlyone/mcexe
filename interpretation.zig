c313b3b14491dcaaf7b105adc44c5c0b142d1f0f
HeDeAn <tannenbaumimjuli@gmail.com>
2024-05-30 17:33:18 +0200

    .

--- interpretation.zig
+++ interpretation.zig
@@ -16,7 +16,7 @@ pub fn mkFile(directory_path: []const u8, file_name: []const u8) !void {
 
 /// Deletes a file or directory and all it's contents at the given path. It recognizes absolute and relative paths.
 pub fn rmFile(directory_path: []const u8, file_name: []const u8, ignore_extension: bool) !void {
-    const directory = try std.fs.cwd().openDir(directory_path, .{.iterate = true});
+    var directory = try std.fs.cwd().openDir(directory_path, .{.iterate = true});
     defer directory.close();
 
     var iterator = directory.iterate();
@@ -25,13 +25,13 @@ pub fn rmFile(directory_path: []const u8, file_name: []const u8, ignore_extensio
         while (try iterator.next()) |file| {
             const ext_index = std.mem.lastIndexOfScalar(u8, file.name, '.');
 
-            if (ext_index != null) {
-                if (std.mem.eql(u8, file_name, file.name[0..ext_index])) {
+            if (ext_index == null) {
+                if (std.mem.eql(u8, file_name, file.name)) {
                     try directory.deleteTree(file.name);
                 }
             }
             else {
-                if (std.mem.eql(u8, file_name, file.name)) {
+                if (std.mem.eql(u8, file_name, file.name[0..ext_index.?])) {
                     try directory.deleteTree(file.name);
                 }
             }
